b5c08159329c22fd40ffa7a9b334ff79d87ba9cb
Houssem Nasri <housi.housi2015@gmail.com>
2021-07-24 17:46:55 +0100

    ok

--- club-service/src/main/java/tech/houssemnasri/clubservice/repositories/ClubRepository.java
+++
@@ -1,7 +0,0 @@
-package tech.houssemnasri.clubservice.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import tech.houssemnasri.clubservice.entities.Club;
-
-public interface ClubRepository extends JpaRepository< Club, Long> {
-}
